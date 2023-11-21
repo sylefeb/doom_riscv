25113d7bc0a60618fda29a7aac76e7dd8eebd978
Sylvain Lefebvre <Sylvain.Lefebvre@inria.fr>
2023-11-21 21:41:31 +0100

    debugging

--- src/r_segs.c
+++ src/r_segs.c
@@ -421,8 +421,8 @@ R_StoreWallRange
         ds_p->scale2 = R_ScaleFromGlobalAngle (viewangle + xtoviewangle[stop]);
         ds_p->scalestep = rw_scalestep =
             (ds_p->scale2 - rw_scale) / (stop-start);
-        printf("%d\n",rw_scalestep); //////////////////////////////////////// required for div to not
-        //////////////////////////////////////////////////// be negative in hardware? investigate!
+        printf("%d = %d/%d\n",rw_scalestep,(ds_p->scale2 - rw_scale),(stop-start)); //////////////////////////////////////// required for div to not
+        ////////////////////////////////////////////////////  investigate!
     }
     else
     {
